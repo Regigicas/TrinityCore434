18a614ea850a9857e549aa9f73151b5f7b3c5e1f
Regigicas <regigicas@hotmail.es>
2018-06-20 16:00:41 +0200

    Update ConditionMgr.h

--- src/server/game/Conditions/ConditionMgr.h
+++ src/server/game/Conditions/ConditionMgr.h
@@ -130,7 +130,7 @@ enum ConditionSourceType
     CONDITION_SOURCE_TYPE_SPELL_PROC                     = 24,
     CONDITION_SOURCE_TYPE_TERRAIN_SWAP                   = 25,
     CONDITION_SOURCE_TYPE_PHASE_DEFINITION               = 26,
-    CONDITION_SOURCE_TYPE_MAX                            = 26  // MAX
+    CONDITION_SOURCE_TYPE_MAX                            = 27  // MAX
 };
 
 enum RelationType
